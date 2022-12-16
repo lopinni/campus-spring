f4433a2f6e5789e18e66968a96e59ab11201c35e
lopinni <lopik98@gmail.com>
2022-12-16 14:27:30 +0100

    Fixed adding products to cart

--- src/main/java/pl/britenet/campus/spring/controller/CartProductController.java
+++ src/main/java/pl/britenet/campus/spring/controller/CartProductController.java
@@ -3,10 +3,17 @@ package pl.britenet.campus.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.britenet.campus.spring.service.AuthService;
+import pl.britenet.campusapiapp.model.Cart;
 import pl.britenet.campusapiapp.model.CartProduct;
+import pl.britenet.campusapiapp.model.Product;
+import pl.britenet.campusapiapp.model.builder.CartBuilder;
+import pl.britenet.campusapiapp.model.builder.CartProductBuilder;
 import pl.britenet.campusapiapp.service.CartProductService;
+import pl.britenet.campusapiapp.service.CartService;
+import pl.britenet.campusapiapp.service.ProductService;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -15,11 +22,18 @@ public class CartProductController {
 
     private final CartProductService cartProductService;
     private final AuthService authService;
+    private final CartService cartService;
+    private final ProductService productService;
 
     @Autowired
-    public CartProductController(CartProductService cartProductService, AuthService authService) {
+    public CartProductController(CartProductService cartProductService,
+                                 AuthService authService,
+                                 CartService cartService,
+                                 ProductService productService) {
         this.cartProductService = cartProductService;
         this.authService = authService;
+        this.cartService = cartService;
+        this.productService = productService;
     }
 
     @GetMapping
@@ -60,5 +74,26 @@ public class CartProductController {
     public void deleteCartProduct(@RequestHeader("Cart-Id") int cartId,
                                   @RequestHeader("Product-Id") int productId) {
         this.cartProductService.deleteCartProduct(cartId, productId);
+    }
+
+    @PostMapping("/add")
+    public void addProductToCart(@RequestHeader("Authorization") String token,
+                                 @RequestHeader("ProductId") int productId) {
+        Product product = this.productService.getProduct(productId);
+        this.cartService.insertCart(new CartBuilder(new Cart())
+                .setUserId(this.authService.getUserId(token))
+                .setOrderDate(new Date())
+                .setTotalPrice(product.getPrice())
+                .setDiscount(0)
+                .getCart());
+        Cart cart = this.cartService.getNewestCartItem();
+        this.cartProductService.insertCartProduct(new CartProductBuilder(new CartProduct())
+                        .setCartId(cart.getId())
+                        .setProductId(product.getId())
+                        .setQuantity(1)
+                        .setProductPrice(product.getPrice())
+                        .setCart(cart)
+                        .setProduct(product)
+                        .getCartProduct());
     }
 }
