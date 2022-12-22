b86492713b4ba1b3aa3dfa12fc9b63885a9e88e3
lopinni <lopik98@gmail.com>
2022-12-22 14:21:47 +0100

    Added returning all products after searching with empty text

--- src/main/java/pl/britenet/campus/spring/controller/ProductController.java
+++ src/main/java/pl/britenet/campus/spring/controller/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @GetMapping(value={"", "/name/"})
     public List<Product> getAll() {
         return this.productService.getAll();
     }
